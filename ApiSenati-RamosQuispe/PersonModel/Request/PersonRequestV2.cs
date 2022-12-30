3c89c6aa7caea0b5d5159214dcf98e6835532827
Crisbagget24 <gusanodeoro6@hotmail.com>
2022-12-29 20:02:03 -0500

    cambio

---
+++ ApiSenati-RamosQuispe/PersonModel/Request/PersonRequestV2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonModel.Request
+{
+    public class PersonRequestV2
+    {
+        public string des { set; get; }
+    }
+}
